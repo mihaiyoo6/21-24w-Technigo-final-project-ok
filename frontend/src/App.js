01489d0bc560bb2dd6028c9cf03d31612480376c
DANIELA <DANIELA.ZACARIAS@OUTLOOK.COM>
2021-06-06 16:14:55 -0500

    FE: Setup of the redux store and the user slice

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Provider } from "react-redux";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { BrowserRouter } from "react-router-dom";
+
+import user from "./reducers/user";
+
+const reducer = combineReducers({
+  user: user.reducer,
+});
+
+const store = configureStore({ reducer });
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Provider store={store}>
+        <div>Hello</div>
+      </Provider>
+    </BrowserRouter>
+  );
+};
---
+++ frontend/src/reusable/urls.js
@@ -0,0 +1,3 @@
+const BASE_URL = 'https://final-project-dannuzak.herokuapp.com'
+
+export const API_URL = (slug) => `${BASE_URL}/${slug}`
