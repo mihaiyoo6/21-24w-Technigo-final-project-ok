7e60a3904c25e4c516b4c0fab19f56b3d521848c
DANIELA <DANIELA.ZACARIAS@OUTLOOK.COM>
2021-06-07 09:50:23 -0500

    Added _redirects file

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,10 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useHistory } from 'react-router-dom'
+import styled from 'styled-components/macro'
 
 import { API_URL } from '../reusable/urls'
 
 import user from '../reducers/user'
+
+const AccessFormWrapper = styled.div`
+ background-color:#E7E4DE;
+ width: 100%;
+ height:200px;
+`
+
+
 
 const Home = () => {
   const [username, setUsername] = useState('')
@@ -22,48 +31,50 @@ const Home = () => {
     }  
 },[accessToken, history])
 
-  const onFormSubmit = (e) =>{
+  const onFormSubmit = (e) => {
       e.preventDefault();
 
       const options = {
-          method: 'POST',
-          headers: {
-              'Content-Type': ' application/json'
-          },
+        method: 'POST',
+        headers: {
+         'Content-Type': ' application/json'
+        },
           body: JSON.stringify({ username, password })
       }
-      fetch(API_URL(mode), options)
+        fetch(API_URL(mode), options)
         .then(res => res.json()) 
         .then(data => {
-            console.log(data)
-            if(data.success){
-                dispatch(user.actions.setUsername(data.username))
-                dispatch(user.actions.setAccessToken(data.accessToken))
-                dispatch(user.actions.setErrors(null));
+          console.log(data)
+          if(data.success){
+            dispatch(user.actions.setUsername(data.username))
+            dispatch(user.actions.setAccessToken(data.accessToken))
+            dispatch(user.actions.setErrors(null));
 
             } else {
-                dispatch(user.actions.setErrors(data))
+              dispatch(user.actions.setErrors(data))
             }
         })
-            .catch()
+        .catch()
     
   }
 
     return (
-        <form onSubmit={onFormSubmit}>
-            <input
-             type="text"
-             value={username}
-             onChange={(e) => setUsername(e.target.value)}
-            /> 
-            <input
-              type="password"
-              value={password}
-              onChange={(e)=> setPassword(e.target.value)}
-            />
-            <button type="submit" onClick={() => setMode('signin')}>Login</button>
-            <button type="submit" onClick={() => setMode('signup')}>Signup</button>
-        </form>
+      <AccessFormWrapper>  
+      <form onSubmit={onFormSubmit}>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        /> 
+        <input
+          type="password"
+          value={password}
+          onChange={(e)=> setPassword(e.target.value)}
+        />
+        <button type="submit" onClick={() => setMode('signin')}>Login</button>
+        <button type="submit" onClick={() => setMode('signup')}>Signup</button>
+      </form>
+      </AccessFormWrapper>  
     )
 }
 
