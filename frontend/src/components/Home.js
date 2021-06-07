db0f3947be815f4f45d036892593d48124644ff8
DANIELA <DANIELA.ZACARIAS@OUTLOOK.COM>
2021-06-06 22:00:59 -0500

    Added local states, fetch and actions to Home component

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,11 +1,56 @@
-import React, { useState} from 'react'
+import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+
+import { API_URL } from '../reusable/urls'
+
+import user from '../reducers/user'
 
 const Home = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [mode, setMode] = useState(null)
+
+  const accessToken = useSelector(store => store.user.accessToken)
+  const dispatch = useDispatch()
+  const history = useHistory()
+
+  useEffect(() => {
+    console.log('checking access token', accessToken)
+    if(accessToken) {
+      history.push('/')
+    }  
+},[accessToken, history])
+
+  const onFormSubmit = (e) =>{
+      e.preventDefault();
+
+      const options = {
+          method: 'POST',
+          headers: {
+              'Content-Type': ' application/json'
+          },
+          body: JSON.stringify({ username, password })
+      }
+      fetch(API_URL(mode), options)
+        .then(res => res.json()) 
+        .then(data => {
+            console.log(data)
+            if(data.success){
+                dispatch(user.actions.setUsername(data.username))
+                dispatch(user.actions.setAccessToken(data.accessToken))
+                dispatch(user.actions.setErrors(null));
+
+            } else {
+                dispatch(user.actions.setErrors(data))
+            }
+        })
+            .catch()
+    
+  }
 
     return (
-        <form>
+        <form onSubmit={onFormSubmit}>
             <input
              type="text"
              value={username}
@@ -16,8 +61,8 @@ const Home = () => {
               value={password}
               onChange={(e)=> setPassword(e.target.value)}
             />
-            <button>Login</button>
-            <button>Signup</button>
+            <button type="submit" onClick={() => setMode('signin')}>Login</button>
+            <button type="submit" onClick={() => setMode('signup')}>Signup</button>
         </form>
     )
 }
