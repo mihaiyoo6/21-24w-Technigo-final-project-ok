90009d4f09da2d088c505374dac759674927e905
DANIELA <DANIELA.ZACARIAS@OUTLOOK.COM>
2021-06-20 13:32:30 -0500

    FE: Basic search bar and return of name and last name

--- frontend/src/components/Resources1.js
+++ frontend/src/components/Resources1.js
@@ -1,54 +1,31 @@
 import React, { useState, useEffect } from 'react'
 
-/* import Resources from "../data.json" */
-
 const Resources1 = () => {
-  const [resources, setResources] = useState([])
-  const [searchTerm, setSearchTerm] = useState('')
-  const [currentCategory, setCurrentCategory] = useState('')
+    const [resources, setResources] = useState([])
+    
 
-     useEffect(() => {
-       console.log(currentCategory)
-      fetch(`https://final-project-dannuzak.herokuapp.com/resources_1?currentCategory=${currentCategory}`)
+    useEffect(() => {
+      fetch("https://final-project-dannuzak.herokuapp.com/resources_1")
         .then((response) => response.json())
         .then((json) => setResources(json))
-  },[currentCategory]) 
-  
+    })
+
     return (
-      <>
-        <button onClick={() => setCurrentCategory("Yoga")}>Yoga</button>
-        <button onClick={() => setCurrentCategory("Reiki")}>Reiki</button>
-        <button onClick={() => setCurrentCategory("Integrative Therapy")}>Integrative Therapy</button>
-          
-        <div>
-          <input 
-            type="text" 
-            placeholder="Search..." 
-            onChange={(event)=> {
-              setSearchTerm(event.target.value)
-            }}
-          />
-            
-          {resources.filter((resource) => {
-            if(searchTerm === '') {
-              return resource
-            } else if(resource.category.toLowerCase().includes(searchTerm.toLowerCase())) {
-              return resource
-            }
-          }).map((resource) => {
-            return (
-              <>
-                <div key={resource._id}>
-                  <div>{resource.first_name}{resource.last_name}</div>
-                  <div>{resource.city}{resource.location}</div>
-                  <div>{resource.remote}</div>
-                  <div>{resource.category}</div>
-                </div>
-              </>
-            )
-          })}
-        </div>
-      </>
+      <div>
+        <input 
+          type="text"       
+          placeholder="Search..." 
+        />
+        {resources.map(resource => {
+          return (
+            <div> 
+              
+                {resource.first_name}{resource.last_name}
+                  
+            </div>
+          )
+        })}
+      </div>
     )
 }
 
