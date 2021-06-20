7583d526ef8a5f892c7340ae146b80df14eaf753
DANIELA <DANIELA.ZACARIAS@OUTLOOK.COM>
2021-06-20 18:50:26 -0500

    Fixed indentation in Resources1 comp

--- frontend/src/components/Resources1.js
+++ frontend/src/components/Resources1.js
@@ -19,30 +19,29 @@ const Resources1 = () => {
           onChange={(event) => {
             setSearchTerm(event.target.value)
           }} 
-          />
-
-            {resources.filter((resource) => {
-              if(searchTerm === '') {
+        />
+          {resources.filter((resource) => {
+            if(searchTerm === '') {
               return resource
             } else if(resource.category.toLowerCase().includes(searchTerm.toLowerCase())) {
               return resource
             }
        
-        }).map((val) => {
-          return (
-            <>
-              <div key={val._id}>
-                <div>{val.first_name} {val.last_name}</div>
-                <div>{val.company}</div>
-                <div>{val.website}</div>
-                <div>{val.email}</div>
-                <div>{val.city} {val.country}</div>
-                <div>{val.modality}</div>
-                <div>{val.category}</div>
-              </div>
-            </>
-          )
-        })}
+            }).map((val) => {
+              return (
+                <>
+                  <div key={val._id}>
+                  <div>{val.first_name} {val.last_name}</div>
+                  <div>{val.company}</div>
+                  <div>{val.website}</div>
+                  <div>{val.email}</div>
+                  <div>{val.city} {val.country}</div>
+                  <div>{val.modality}</div>
+                  <div>{val.category}</div>
+                </div>
+                </>
+              )
+            })}
       </div>
     )
 }
