771c956a90344e44bb6df7c0631ac3ed07fd6529
DANIELA <DANIELA.ZACARIAS@OUTLOOK.COM>
2021-06-20 20:16:50 -0500

    Search bar working, buttons not working yet

--- frontend/src/components/Resources1.js
+++ frontend/src/components/Resources1.js
@@ -1,52 +1,53 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+
+/* import Resources from "../data.json" */
 
 const Resources1 = () => {
   const [resources, setResources] = useState([])
-  const [filteredResources, setFilteredResources] = useState([])
   const [searchTerm, setSearchTerm] = useState('')
+  const [currentCategory, setCurrentCategory] = useState('')
 
-  const getResources = (category) => {
-    fetch(`https://final-project-dannuzak.herokuapp.com/resources_1?currentCategory=${category}`)
-      .then((response) => response.json())
-      .then((json) => setResources(json))
-  }
-
-  const filterResources = (searchTerm) => {
-    if (searchTerm === "") {
-      setFilteredResources(resources)
-    } else {
-      const filteredArray = resources.filter((resource) => resource.category.toLowerCase().includes(searchTerm.toLowerCase()))
-      setFilteredResources(filteredArray)
-    }
-  }
-
-  return (
-    <>
-      <button onClick={() => getResources("Yoga")}>Yoga</button>
-      <button onClick={() => getResources("Reiki")}>Reiki</button>
-      <button onClick={() => getResources("Integrative Therapy")}>Integrative Therapy</button>
-
-      <div>
-        <input
-          type="text"
-          placeholder="Search..."
-          onChange={(event) => {
-            setSearchTerm(event.target.value)
-          }}
-        />
-        <button onClick={() => filterResources(searchTerm)} >Search</button>
-
-        {filteredResources.map((resource) => (
-          <div key={resource._id}>
-            <div>{resource.first_name}{resource.last_name}</div>
-            <div>{resource.city}{resource.location}</div>
-            <div>{resource.remote}</div>
-            <div>{resource.category}</div>
-          </div>
-        ))}
-      </div>
-    </>
-  )
+     useEffect(() => {
+       console.log(currentCategory)
+      fetch("https://final-project-dannuzak.herokuapp.com/resources_1")
+        .then((response) => response.json())
+        .then((json) => setResources(json))
+  },[currentCategory]) 
+  
+    return (
+      <>
+        <button onClick={() => setCurrentCategory("Yoga")}>Yoga</button>
+        <button onClick={() => setCurrentCategory("Reiki")}>Reiki</button>
+        <button onClick={() => setCurrentCategory("Integrative Therapy")}>Integrative Therapy</button>
+          <input 
+            type="text" 
+            placeholder="Search..." 
+            onChange={(event)=> {
+              setSearchTerm(event.target.value)
+            }}
+          />
+            <div>
+              {resources.filter((resource) => {
+                if(searchTerm === '') {
+                  return resource
+                } else if(resource.category.toLowerCase().includes(searchTerm.toLowerCase())) {
+                  return resource
+              }
+              }).map((resource) => {
+                return (
+                  <>
+                    <div key={resource._id}>
+                      <div>{resource.first_name}{resource.last_name}</div>
+                      <div>{resource.city}{resource.location}</div>
+                      <div>{resource.remote}</div>
+                      <div>{resource.category}</div>
+                    </div>
+                  </>
+                )
+              })}
+            </div> 
+          </>
+    )
 }
 
 export default Resources1
