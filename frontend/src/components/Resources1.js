94d12319ebd3cca818311197bc70c9e3a8268359
DANIELA <DANIELA.ZACARIAS@OUTLOOK.COM>
2021-06-20 19:37:39 -0500

    FE: Testing Alternative solution for buttons and searchbar

--- frontend/src/components/Resources1.js
+++ frontend/src/components/Resources1.js
@@ -1,49 +1,52 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 
 const Resources1 = () => {
-    const [resources, setResources] = useState([])
-    const [searchTerm, setSearchTerm] = useState('')
-    
+  const [resources, setResources] = useState([])
+  const [filteredResources, setFilteredResources] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
 
-    useEffect(() => {
-      fetch("https://final-project-dannuzak.herokuapp.com/resources_1")
-        .then((response) => response.json())
-        .then((json) => setResources(json))
-    })
+  const getResources = (category) => {
+    fetch(`https://final-project-dannuzak.herokuapp.com/resources_1?currentCategory=${category}`)
+      .then((response) => response.json())
+      .then((json) => setResources(json))
+  }
 
-    return (
+  const filterResources = (searchTerm) => {
+    if (searchTerm === "") {
+      setFilteredResources(resources)
+    } else {
+      const filteredArray = resources.filter((resource) => resource.category.toLowerCase().includes(searchTerm.toLowerCase()))
+      setFilteredResources(filteredArray)
+    }
+  }
+
+  return (
+    <>
+      <button onClick={() => getResources("Yoga")}>Yoga</button>
+      <button onClick={() => getResources("Reiki")}>Reiki</button>
+      <button onClick={() => getResources("Integrative Therapy")}>Integrative Therapy</button>
+
       <div>
-        <input 
-          type="text"       
+        <input
+          type="text"
           placeholder="Search..."
           onChange={(event) => {
             setSearchTerm(event.target.value)
-          }} 
+          }}
         />
-          {resources.filter((resource) => {
-            if(searchTerm === '') {
-              return resource
-            } else if(resource.category.toLowerCase().includes(searchTerm.toLowerCase())) {
-              return resource
-            }
-       
-            }).map((val) => {
-              return (
-                <>
-                  <div key={val._id}>
-                  <div>{val.first_name} {val.last_name}</div>
-                  <div>{val.company}</div>
-                  <div>{val.website}</div>
-                  <div>{val.email}</div>
-                  <div>{val.city} {val.country}</div>
-                  <div>{val.modality}</div>
-                  <div>{val.category}</div>
-                </div>
-                </>
-              )
-            })}
+        <button onClick={() => filterResources(searchTerm)} >Search</button>
+
+        {filteredResources.map((resource) => (
+          <div key={resource._id}>
+            <div>{resource.first_name}{resource.last_name}</div>
+            <div>{resource.city}{resource.location}</div>
+            <div>{resource.remote}</div>
+            <div>{resource.category}</div>
+          </div>
+        ))}
       </div>
-    )
+    </>
+  )
 }
 
 export default Resources1
