1163395c4c5dc1eae224438fada33ac5440e36dc
DANIELA <DANIELA.ZACARIAS@OUTLOOK.COM>
2021-06-22 11:46:32 -0500

    Fetch of list of  positive thoughts is working

--- frontend/src/pages/PositiveSharing.js
+++ frontend/src/pages/PositiveSharing.js
@@ -1,23 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 
 import { API_URL_POS_SHARING } from '../reusable/urls'
 
 const PositiveSharing = () => {
+  const [positiveThoughtsList, setPositiveThoughtsList] = useState([])
 
+useEffect(() => {
+  fetchPositiveThoughts()
+},[])
 
   const fetchPositiveThoughts = () => {
     fetch(API_URL_POS_SHARING)
      .then(res => res.json())
-     .then(thoughts => console.log(thoughts))
+     .then(thoughts => setPositiveThoughtsList(thoughts))
      .catch(err => console.error(err))
 
   }
 
-  fetchPositiveThoughts()
-
-    return (
+     return (
       <div>
-        Positive Sharing
+        {positiveThoughtsList.map(thought =>(
+          <div key={thought._id}>
+            <h4>{thought.message}</h4>
+          </div>
+        ))}
       </div>
     )
 }
