3ed107bf42a5c177650df7d470f11b70b3db8a48
DANIELA <DANIELA.ZACARIAS@OUTLOOK.COM>
2021-06-22 12:54:05 -0500

    FE: Added form for new PositiveThought and on submit button, without fetch

--- frontend/src/pages/PositiveSharing.js
+++ frontend/src/pages/PositiveSharing.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect} from 'react'
+import moment from 'moment'
 
 import { API_URL_POS_SHARING } from '../reusable/urls'
 
 const PositiveSharing = () => {
   const [positiveThoughtsList, setPositiveThoughtsList] = useState([])
+  const  [newPositiveThought, setNewPositiveThought] = useState('')
 
 useEffect(() => {
   fetchPositiveThoughts()
@@ -14,14 +16,35 @@ useEffect(() => {
      .then(res => res.json())
      .then(thoughts => setPositiveThoughtsList(thoughts))
      .catch(err => console.error(err))
+  }
 
+  const onNewPositiveThoughtChange = (event) => {
+    setNewPositiveThought(event.target.value)
+
+  }
+
+  const onFormSubmit =(event) =>{
+    event.preventDefault()
+    console.log('Form submitted', newPositiveThought)
   }
 
      return (
       <div>
+        <form onSubmit={onFormSubmit}>
+          <label htmlFor="newPositiveThought">Share a positive thought!</label>
+          <input
+           id="newPositiveThought"
+           type="text"
+           value={newPositiveThought}
+           onChange={onNewPositiveThoughtChange}
+          />
+        <button type="onSubmit">Send</button>
+        </form> 
+        
         {positiveThoughtsList.map(thought =>(
           <div key={thought._id}>
             <h4>{thought.message}</h4>
+            <p>{moment(thought.created).fromNow()}</p>
           </div>
         ))}
       </div>
