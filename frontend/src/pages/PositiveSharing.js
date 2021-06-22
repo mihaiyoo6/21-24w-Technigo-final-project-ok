3418e8fc01463bd4c2b1273193fc686b06acc4d4
DANIELA <DANIELA.ZACARIAS@OUTLOOK.COM>
2021-06-22 17:40:35 -0500

    Commented out everything about thumbsup in Positive Sharing comp

--- frontend/src/pages/PositiveSharing.js
+++ frontend/src/pages/PositiveSharing.js
@@ -1,82 +1,82 @@
-import React, { useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import moment from 'moment'
 
-import { API_URL_POS_SHARING, THUMBSUP_URL } from '../reusable/urls'
+import { API_URL_POS_SHARING, /* THUMBSUP_URL */ } from '../reusable/urls'
 
 const PositiveSharing = () => {
   const [positiveThoughtsList, setPositiveThoughtsList] = useState([])
-  const  [newPositiveThought, setNewPositiveThought] = useState('')
+  const [newPositiveThought, setNewPositiveThought] = useState('')
 
-useEffect(() => {
-  fetchPositiveThoughts()
-},[])
+  useEffect(() => {
+    fetchPositiveThoughts()
+  }, [])
 
   const fetchPositiveThoughts = () => {
     fetch(API_URL_POS_SHARING)
-     .then(res => res.json())
-     .then(thoughts => setPositiveThoughtsList(thoughts))
-     .catch(err => console.error(err))
+      .then(res => res.json())
+      .then(thoughts => setPositiveThoughtsList(thoughts))
+      .catch(err => console.error(err))
   }
 
   const onNewPositiveThoughtChange = (event) => {
     setNewPositiveThought(event.target.value)
   }
 
-  const onFormSubmit =(event) => {
+  const onFormSubmit = (event) => {
     event.preventDefault()
 
     const options = {
-      method:'POST',
-      headers:{
+      method: 'POST',
+      headers: {
         'Content-type': 'application/json'
       },
-      body: JSON.stringify({ message:newPositiveThought})
+      body: JSON.stringify({ message: newPositiveThought })
     }
 
     fetch(API_URL_POS_SHARING, options)
       .then(res => res.json())
       .then(() => fetchPositiveThoughts())
       .catch(err => console.error(err))
-      setNewPositiveThought('')
+    setNewPositiveThought('')
   }
 
-  const onThumbsupIncrease = (_id) => {
+ /*  const onThumbsupIncrease = (_id) => {
     const options = {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json'
-    },
-  }
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+    }
     fetch(THUMBSUP_URL(_id), options)
-     .then(res => res.json())
-     .then(() => fetchPositiveThoughts())
-     .catch(err => console.error(err))
-}
+      .then(res => res.json())
+      .then(() => fetchPositiveThoughts())
+      .catch(err => console.error(err)) 
+  }  */
 
-     return (
-      <div>
-        <form onSubmit={onFormSubmit}>
-          <label htmlFor="newPositiveThought">Share a positive thought!</label>
-          <input
-           id="newPositiveThought"
-           type="text"
-           value={newPositiveThought}
-           onChange={onNewPositiveThoughtChange}
-          />
+  return (
+    <div>
+      <form onSubmit={onFormSubmit}>
+        <label htmlFor="newPositiveThought">Share a positive thought!</label>
+        <input
+          id="newPositiveThought"
+          type="text"
+          value={newPositiveThought}
+          onChange={onNewPositiveThoughtChange}
+        />
         <button type="onSubmit">Send</button>
-        </form> 
-        
-        {positiveThoughtsList.map(thought =>(
-          <div key={thought._id}>
-            <h4>{thought.message}</h4>
-            <p>{moment(thought.created).fromNow()}</p>
-            <button onClick={onThumbsupIncrease(thought._id)}>
-              {thought.thumbsup}👍
-            </button>
-          </div>
-        ))}
-      </div>
-    )
+      </form>
+
+      {positiveThoughtsList.map(thought => (
+        <div key={thought._id}>
+          <h4>{thought.message}</h4>
+          <p>{moment(thought.created).fromNow()}</p>
+          {/* <button onClick={onThumbsupIncrease(thought._id)}>
+            {thought.thumbsup}👍
+          </button>  */}
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default PositiveSharing
