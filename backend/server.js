5d7312f6e4f3ffd45d31a74d26e290df0ea1be32
Daniela Zacarias <daniela.zacarias@outlook.com>
2023-06-20 14:41:09 +0100

    fixed dependency issues

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebase = require('firebase/app');
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBeE8KfMO2IAAe6YMv5IkotncXqcmK39n4",
+  authDomain: "final-project-technigo-5bf9b.firebaseapp.com",
+  projectId: "final-project-technigo-5bf9b",
+  storageBucket: "final-project-technigo-5bf9b.appspot.com",
+  messagingSenderId: "126560165961",
+  appId: "1:126560165961:web:5582f150c2e204312c9f98",
+  measurementId: "G-9HWQHY80ZE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
